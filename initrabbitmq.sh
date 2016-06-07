4c27212c2863c84f7578a3b4aaedc4b15a0e9f1f
Basilio Vera <basilio.vera@softonic.com>
2016-06-07 10:30:24 +0200

    RabbitMQ process has initiated with 'exec'

--- initrabbitmq.sh
+++ initrabbitmq.sh
@@ -2,7 +2,7 @@
 set -o monitor
 
 rabbit_pid=/var/lib/rabbitmq/mnesia/rabbitmq.pid
-RABBITMQ_PID_FILE=$rabbit_pid /docker-entrypoint.sh "$@" &
+RABBITMQ_PID_FILE=$rabbit_pid exec /docker-entrypoint.sh "$@" &
 
 rabbitmqctl wait $rabbit_pid
 
