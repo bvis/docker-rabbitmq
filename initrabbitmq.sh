cb2677454c9d4915a8754049bcb9c92459f10ffa
Basilio Vera <basilio.vera@softonic.com>
2018-06-25 13:22:28 +0200

    Updated RabbitMQ version

--- initrabbitmq.sh
+++ initrabbitmq.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -o monitor
 
+: ${RABBIT_SLEEP_SECONDS:=5}
+
 rabbit_pid=/var/lib/rabbitmq/mnesia/rabbitmq.pid
 RABBITMQ_PID_FILE=$rabbit_pid exec /docker-entrypoint.sh "$@" &
+
+sleep ${RABBIT_SLEEP_SECONDS}
 
 rabbitmqctl wait $rabbit_pid
 
@@ -16,5 +20,4 @@ echo
     done
 
 jobs
-
 fg %1
